36b0bede466c7164230487ec6a11714985d60af3
Rajiv Makhijani <rajiv@blue-tech.org>
2015-07-29 15:58:52 -0700

    Replace incrementing id with UUID generator

--- linky/utils/shortener.py
+++ linky/utils/shortener.py
@@ -1,16 +1,22 @@
 # coding=utf-8
 from __future__ import absolute_import, unicode_literals, print_function
 
+from linky.utils.datastore import KeyAlreadyExistsError
+import shortuuid
+
 
 class Shortener(object):
     def __init__(self, datastore):
         self.datastore = datastore
-        self.next_id = 1
 
     def shorten(self, url):
-        key = unicode(self.next_id)
-        self.datastore.put(key, url)
-        self.next_id += 1
+        while True:
+            try:
+                key = shortuuid.uuid()[:7]
+                self.datastore.put(key, url)
+                break
+            except KeyAlreadyExistsError:
+                pass
         return key
 
     def lookup(self, key):
